43fb3c654b88bf4e391b346c033c0e660c3cfb2f
sts19813 <sts19813@gmail.com>
2026-03-30 09:57:06 -0600

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,47 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('partials.layout.head')
-<body id="kt_app_body"
-    data-kt-app-header-fixed="true"
-    data-kt-app-header-fixed-mobile="true"
-    data-kt-app-sidebar-enabled="true"
-    data-kt-app-sidebar-fixed="true"
-    data-kt-app-sidebar-hoverable="true"
-    data-kt-app-sidebar-push-toolbar="true"
-    data-kt-app-sidebar-push-footer="true"
-    data-kt-app-aside-enabled="true"
-    data-kt-app-aside-fixed="true"
-    data-kt-app-aside-push-toolbar="true"
-    data-kt-app-aside-push-footer="true"
+
+<body id="kt_app_body" data-kt-app-header-fixed="true" data-kt-app-header-fixed-mobile="true"
+    data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true"
+    data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-aside-enabled="true"
+    data-kt-app-aside-fixed="true" data-kt-app-aside-push-toolbar="true" data-kt-app-aside-push-footer="true"
     class="app-default">
 
     <!--begin::Theme mode setup on page load-->
-    <script>
-        var defaultThemeMode = "light";
-        var themeMode;
+    <script>var defaultThemeMode = "light"; var themeMode; if (document.documentElement) { if (document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if (localStorage.getItem("data-bs-theme") !== null) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
 
-        if (document.documentElement) {
-            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
-                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
-            } else if (localStorage.getItem("data-bs-theme") !== null) {
-                themeMode = localStorage.getItem("data-bs-theme");
-            } else {
-                themeMode = defaultThemeMode;
-            }
-
-            if (themeMode === "system") {
-                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-            }
-
-            document.documentElement.setAttribute("data-bs-theme", themeMode);
-        }
-    </script>
     <!--end::Theme mode setup on page load-->
 
     <!--begin::App-->
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
-        <!--begin::Page-->
-        <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
+		<!--begin::Page-->
+		<div class="app-page flex-column flex-column-fluid" id="kt_app_page">
+			<!--begin::Header-->
+			
             @include('partials.layout.header')
 
             <!--begin::Wrapper-->
@@ -59,11 +36,11 @@
                                 @isset($slot)
                                     {{ $slot }}
                                 @else
-                                    @hasSection('content')
-                                        @yield('content')
-                                    @else
-                                        @include('partials.layout.content')
-                                    @endif
+                                @hasSection('content')
+                                    @yield('content')
+                                @else
+                                    @include('partials.layout.content')
+                                @endif
                                 @endif
                             </div>
                             <!--end::Content container-->
@@ -88,4 +65,5 @@
     @include('partials.layout.modals')
     @include('partials.layout.scripts')
 </body>
+
 </html>
